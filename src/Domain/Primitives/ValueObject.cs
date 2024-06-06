f980d764d1853de352a5f812ec1b755a523ddb82
Markus Iorio <markus.iorio@bfd.de>
2024-06-06 13:31:43 +0200

    operator

--- src/Domain/Primitives/ValueObject.cs
+++ src/Domain/Primitives/ValueObject.cs
@@ -4,6 +4,20 @@ public abstract class ValueObject
 {
     public abstract IEnumerable<object> GetAtomicValues();
 
+    public static bool operator ==(ValueObject? left, ValueObject? right)
+    {
+        if (left is null)
+            return right is null;
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(ValueObject? left, ValueObject? right)
+    {
+        return !(left == right);
+    }
+
+
     public bool Equals(ValueObject? other) => other is not null && ValuesAreEqual(other);
 
     public override bool Equals(object? obj) => obj is ValueObject other && ValuesAreEqual(other);
