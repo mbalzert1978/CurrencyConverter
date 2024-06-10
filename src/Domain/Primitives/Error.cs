07e4d00201465092ff4162f56e5b55a0793ffe4d
Markus Iorio <markus.iorio@bfd.de>
2024-06-10 08:51:03 +0200

    introduce StatusCode enum

--- src/Domain/Primitives/Error.cs
+++ src/Domain/Primitives/Error.cs
@@ -2,8 +2,18 @@ namespace Domain;
 
 public sealed record Error(string Code, string? Description = null)
 {
+
     public static readonly Error None = new(string.Empty, string.Empty);
 
     public static readonly Exception UnreachableException =
         new("Unreachable should not happen.", new InvalidOperationException("500"));
+}
+
+public static class StatusCode
+{
+    public const string BadRequest = "400";
+    public const string NotFound = "404";
+    public const string InternalServerError = "500";
+    public const string UnprocessableEntity = "422";
+    public const string Conflict = "409";
 }
